fe78fdf9e59cc5b562ff044a483a8bd85d7b6ae4
root <fkdrms123@naver.com>
2025-06-19 12:34:40 +0900

    intro ok

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,14 @@
 export function menu() {
-    console.log(menu)
+  const headerToggle = document.getElementById("headerToggle");
+  const headerNav = document.querySelector(".header__nav");
+
+  if (headerToggle) {
+    headerToggle.addEventListener("click", () => {
+      headerNav.classList.toggle("show");
+
+      const expanded = headerToggle.getAttribute("aria-expanded") === "true";
+      headerToggle.setAttribute("aria-expanded", expanded ? "false" : "true");
+    });
+  }
 }
+
